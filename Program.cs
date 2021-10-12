f6fbddd6ddc0914eb3153f46751d8b05a9e916ae
Yasmin Alves <yasilvalves@gmail.com>
2021-10-12 18:00:29 -0300

    Alterações: opção saída e chamar menu recorrentemente

--- Program.cs
+++ Program.cs
@@ -6,25 +6,10 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            short option = Menu();
-
-            Console.WriteLine("Primeiro valor: ");
-            float v1 = float.Parse(Console.ReadLine());
-
-            Console.WriteLine("Segundo valor: ");
-            float v2 = float.Parse(Console.ReadLine());
-
-            switch (option)
-            {
-                case 1: Soma(v1, v2); break;
-                case 2: Subtracao(v1, v2); break;
-                case 3: Multiplicacao(v1, v2); break;
-                case 4: Divisao(v1, v2); break;
-                default: Console.WriteLine("Opção inválida!"); break;
-            }
+            Menu();
         }
 
-        static short Menu () {
+        static void Menu () {
             Console.Clear(); //limpar tela
 
             Console.WriteLine("Qual operação realizar?");
@@ -32,31 +17,74 @@ namespace Calculator
             Console.WriteLine("2 - Subtração");
             Console.WriteLine("3 - Multiplicação");
             Console.WriteLine("4 - Divisão");
+            Console.WriteLine("5 - Sair");
 
             Console.WriteLine("Selecione a opção: ");
             short option = short.Parse(Console.ReadLine());
 
             Console.Clear(); //limpar tela
 
-            return option;
+            switch (option)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Multiplicacao(); break;
+                case 4: Divisao(); break;
+                case 5: System.Environment.Exit(0); break; //Processo finalizado, retorna um código de êxito para o sistema
+                default: Menu(); break;
+            }
+
         }
 
-        static void Soma (float v1, float v2) {
+        static void Soma () {
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
             float resultado = v1 + v2;
             Console.WriteLine("O resultado da soma é: " + resultado);
+            Console.ReadKey(); //Para o sistema mostrar o resultado antes de abrir o menu novamente
+            Menu();
         }
-        static void Subtracao (float v1, float v2) {
+        static void Subtracao () {
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
             float resultado = v1 - v2;
             Console.WriteLine("O resultado da subtração é: " + resultado);
+            Console.ReadKey();
+            Menu();
         }
-        static void Divisao (float v1, float v2) {
+        static void Divisao () {
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
             float resultado = v1 / v2;
             Console.WriteLine("O resultado da divisão é: " + resultado);
+            Console.ReadKey();
+            Menu();
         }
-        static void Multiplicacao (float v1, float v2) {
+        static void Multiplicacao () {
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
             float resultado = v1 * v2;
             Console.WriteLine("O resultado da multiplicação é: " + resultado);
+            Console.ReadKey();
+            Menu();
         }
+
 
     }
 }
